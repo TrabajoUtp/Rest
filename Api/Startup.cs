06b4dedc8b9e57e4f9d84a90087f769786a4a4da
Frank Cochachin <frankronald12345@gmail.com>
2018-10-23 01:22:52 -0500

    Api

---
+++ Api/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+
+[assembly: OwinStartup(typeof(Api.Startup))]
+
+namespace Api
+{
+    public class Startup
+    {
+        public static OAuthBearerAuthenticationOptions OAuthBearerOptions { get; private set; }
+        public void Configuration(IAppBuilder app)
+        {
+            // Para obtener más información acerca de cómo configurar su aplicación, visite http://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCors(CorsOptions.AllowAll);
+            ConfigureOAuth(app);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthBearerOptions = new OAuthBearerAuthenticationOptions();
+            var oAuthServerOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(3),
+                Provider = new MyAuthAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(oAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                AuthenticationType = "Bearer"
+            });
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
